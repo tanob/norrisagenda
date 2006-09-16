be90d46ce32a278dc9ba1c62a57edc98b1002f34
Adriano Bonat <tanob@users.noreply.github.com>
2006-09-16 08:02:26 +0000

    Manipulacao de arvores balanceadas (AVL) implementada.

--- src/lib/structure.h
+++ src/lib/structure.h
@@ -6,22 +6,56 @@
 
 #define _STRUCTURE_H
 
+/**
+	Estrutura para os dados do contato
+ */
+
+typedef struct _Contato Contato;
+
+struct _Contato {
+	char nome[50];
+	char telefone[30];
+};
+
+/**
+	Define constantes para trabalhar com os fatores de balanceamento
+ */
+#define BAL 0 /* Esta balanceada */
+#define BD 1 /* Balanceada para direita */
+#define BE -1 /* Balanceada para esquerda */
+
+/**
+	Estrutura para comportar tanto uma arvore balanceada, quanto
+	uma nao-balanceada
+ */
+typedef struct _ArvoreMista ArvoreMista;
+
+struct _ArvoreMista {
+	Contato *contato;
+	ArvoreMista *esq;
+	ArvoreMista *dir;
+	short int fb; /* Usado apenas quando for balanceada */
+	char ehAVL; /* Indica se a arvore eh AVL */
+};
+
+/**
+	Estrutura util para se reunir os dados usados para uma agenda
+ */
 typedef struct _AgendaInfo {
 	FILE *arquivo; /* Manipulador do arquivo */
 
 	char ehAVL; /* Indica se eh balanceada ou nao */
 	char foiAlterada; /* Se a agenda foi aberta e alterada e suas alteracoes ainda nao foram salvas */
 
-	/*
-		A ideia eh guardar alguma outra estrutura de dados aqui,
-		como uma estrutura que valha tanto para a AVL qto para a nao-AVL,
-		talvez um "union" dessas estruturas...
-	*/
+	ArvoreMista *arv;
 
 } AgendaInfo;
 
 
 AgendaInfo *inicAgendaInfo();
 void finalizaAgendaInfo(AgendaInfo *);
+void finalizaArvoreMista(ArvoreMista *);
+ArvoreMista *insereNoh(ArvoreMista *, ArvoreMista *);
+int removeNoh(const char *, ArvoreMista **);
 
 #endif /* !_STRUCTURE_H */
